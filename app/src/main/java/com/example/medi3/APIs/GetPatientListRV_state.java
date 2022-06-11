527b80317405f9ef618f0c459faa15a39d34e736
sagarck07 <shubhamsagar575@gmail.com>
2022-06-11 18:13:22 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ app/src/main/java/com/example/medi3/APIs/GetPatientListRV_state.java
@@ -0,0 +1,20 @@
+package com.example.medi3.APIs;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class GetPatientListRV_state {
+    private static final String url = "http://192.168.50.111:8080/patient_list/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getRetrofit() {
+
+        if (retrofit == null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(url)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
