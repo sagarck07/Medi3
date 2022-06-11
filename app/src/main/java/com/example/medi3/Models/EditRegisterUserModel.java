60cfc6478a4f46cdcacf6ac27fff736162608dfd
sagarck07 <shubhamsagar575@gmail.com>
2022-06-11 18:13:22 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ app/src/main/java/com/example/medi3/Models/EditRegisterUserModel.java
@@ -0,0 +1,53 @@
+package com.example.medi3.Models;
+
+public class EditRegisterUserModel {
+    String name, blood_group, state, city, age;
+
+    public EditRegisterUserModel(String name, String blood_group, String state, String city, String age) {
+        this.name = name;
+        this.blood_group = blood_group;
+        this.state = state;
+        this.city = city;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
